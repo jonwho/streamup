614d096b2d4fc642ea9a38f7d889d2d58e360159
Jon Ho <jonwho.contact@gmail.com>
2017-07-15 21:52:49 -0700

    hmm cant get auth to work with pow hosting, just use rails server for now

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+# TODO: change production host once heroku is set up
+OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_OAUTH2_CLIENTID'], ENV['GOOGLE_OAUTH2_SECRET']
 end
