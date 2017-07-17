e0aa2ed9beebaf1f611e30780fd1391e156c7980
Jon Ho <jonwho.contact@gmail.com>
2017-07-16 18:27:23 -0700

    web socket auth for another time perhaps

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,9 +1,14 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # WebSocket cant read from session middleware variable
+    # but it can read cookies from client
+    # should add token auth
+
     #include SessionsHelper
 
     #identified_by :message_user
 
+    # use this method when you want to add authorization for connecting to ActionCable
     #def connect
       #self.message_user = find_verified_user
     #end
