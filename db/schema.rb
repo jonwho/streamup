0b274d38998128f7276bd80019beac1c80af3b9a
Jon Ho <jonwho.contact@gmail.com>
2017-07-16 18:28:53 -0700

    username migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170716113306) do
+ActiveRecord::Schema.define(version: 20170717012820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170716113306) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.string   "email"
+    t.string   "username"
   end
 
   add_foreign_key "messages", "users"
