a19d7b6618aa27b3652eed0632330e7d9d667af5
Jon Ho <jonwho.contact@gmail.com>
2017-07-17 03:33:39 -0700

    keep same username each time a user logs in

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,19 @@ class User < ApplicationRecord
     uniqueness: { case_sensitive: false }
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.email = auth.info.email
-      user.username = User.random_username
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
+    if pre_existing_user = User.find_by_email(auth.info.email)
+      pre_existing_user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.email = auth.info.email
+        user.username = User.random_username
+        user.oauth_token = auth.credentials.token
+        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+        user.save!
+      end
     end
   end
 
