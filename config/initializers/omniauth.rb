60d393b8660cc133c5200a41677671b337041940
Jon Ho <jonwho.contact@gmail.com>
2017-07-16 19:52:10 -0700

    heroku prod

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,14 @@
-# TODO: change production host once heroku is set up
-OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://jonwho-streamup.herokuapp.com' : 'http://localhost:3000'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_OAUTH2_CLIENTID'], ENV['GOOGLE_OAUTH2_SECRET']
+  provider :google_oauth2,
+    ENV['GOOGLE_OAUTH2_CLIENTID'],
+    ENV['GOOGLE_OAUTH2_SECRET']
+    #{
+      #client_options: {
+        #ssl: {
+          #ca_file: '/usr/lib/ssl/certs/ca-certificates.crt'
+        #}
+      #}
+    #}
 end
