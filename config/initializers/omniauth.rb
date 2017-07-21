d351fb08c89c050e59cce42953565abc43c1507f
Jon Ho <jonwho.contact@gmail.com>
2017-07-21 10:45:25 -0400

    remove comment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,11 +4,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
     ENV['GOOGLE_OAUTH2_CLIENTID'],
     ENV['GOOGLE_OAUTH2_SECRET']
-    #{
-      #client_options: {
-        #ssl: {
-          #ca_file: '/usr/lib/ssl/certs/ca-certificates.crt'
-        #}
-      #}
-    #}
 end
